01f98c3f908095e52854b756e7205e051bf00686
Rizjami Putera <rizjamiputera@gmail.com>
2024-03-02 01:29:49 +0700

    TODO extend token expire

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -39,6 +39,8 @@ export class AuthGuard implements CanActivate {
             // 💡 We're assigning the payload to the request object here
             // so that we can access it in our route handlers
             request['user'] = payload;
+
+            // TODO update token
         } catch {
             throw new UnauthorizedException();
         }
