de5aaf48c9d8221211a470b8cb4a32bb80175d4f
Rizjami Putera <rizjamiputera@gmail.com>
2024-03-26 13:19:14 +0700

    disable delete by update

--- src/articles/dto/update-article.dto.ts
+++ src/articles/dto/update-article.dto.ts
@@ -2,17 +2,12 @@ import { PartialType } from "@nestjs/mapped-types";
 import { IsArray, IsBoolean, IsOptional } from "class-validator";
 import { CreateArticleDto } from "./create-article.dto";
 
-interface updateAPhotoDto {
+interface updatePhotoDto {
     id: string
     index: number
 }
 
 export class UpdateArticleDto extends PartialType(CreateArticleDto) {
-    @IsBoolean()
-    @IsOptional()
-    deleted: boolean
-
     @IsArray()
-    @IsOptional()
-    photos: updateAPhotoDto[]
+    photos: updatePhotoDto[]
 }
--- src/itikaf_schedules/dto/update-itikaf_schedule.dto.ts
+++ src/itikaf_schedules/dto/update-itikaf_schedule.dto.ts
@@ -1,11 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateItikafScheduleDto } from './create-itikaf_schedule.dto';
-import { IsArray, IsBoolean } from 'class-validator';
+import { IsArray, IsBoolean, IsOptional } from 'class-validator';
 
 export class UpdateItikafScheduleDto extends PartialType(CreateItikafScheduleDto) {
-    @IsBoolean()
-    deleted: boolean
-
     @IsArray()
     photos: {
         id: string,
