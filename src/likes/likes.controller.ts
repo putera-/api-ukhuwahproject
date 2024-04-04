780b203afcfc314b7374e91060dcd1d953b3224b
Rizjami Putera <rizjamiputera@gmail.com>
2024-04-05 05:30:21 +0700

    add like & unline api for itikaf, itikaf schedule, article, comment, & comment reply

--- src/likes/likes.controller.ts
+++ src/likes/likes.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, HttpCode } from '@nestjs/common';
 import { LikesService } from './likes.service';
 import { CreateLikeDto } from './dto/create-like.dto';
 import { UpdateLikeDto } from './dto/update-like.dto';
@@ -31,4 +31,109 @@ export class LikesController {
   // remove(@Param('id') id: string) {
   //   return this.likesService.remove(+id);
   // }
+
+  // LIKE
+
+  @Post('/itikaf/:id')
+  @HttpCode(204)
+  async likeItikaf(@Req() req, @Param('id') id: string): Promise<void> {
+    try {
+      return this.likesService.likeItikaf(id, req.user.id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Post('/itikaf-schedule/:id')
+  @HttpCode(204)
+  async likeItikafSchedule(@Req() req, @Param('id') id: string): Promise<void> {
+    try {
+      return this.likesService.likeItikafSchedule(id, req.user.id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Post('/article/:id')
+  @HttpCode(204)
+  async likeArticle(@Req() req, @Param('id') id: string): Promise<void> {
+    try {
+      return this.likesService.likeArticle(id, req.user.id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Post('/comment/:id')
+  @HttpCode(204)
+  async likeComment(@Req() req, @Param('id') id: string): Promise<void> {
+    try {
+      return this.likesService.likeComment(id, req.user.id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Post('/comment-reply/:id')
+  @HttpCode(204)
+  async likeCommentReply(@Req() req, @Param('id') id: string): Promise<void> {
+    try {
+      return this.likesService.likeCommentReply(id, req.user.id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // UNLIKE
+
+  @Delete('/itikaf/:id')
+  @HttpCode(204)
+  async unLikeItikaf(@Req() req, @Param('id') id: string): Promise<void> {
+    try {
+      return this.likesService.unLikeItikaf(id, req.user.id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Delete('/itikaf-schedule/:id')
+  @HttpCode(204)
+  async unLikeItikafSchedule(@Req() req, @Param('id') id: string): Promise<void> {
+    try {
+      return this.likesService.unLikeItikafSchedule(id, req.user.id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Delete('/article/:id')
+  @HttpCode(204)
+  async unLikeArticle(@Req() req, @Param('id') id: string): Promise<void> {
+    try {
+      return this.likesService.unLikeArticle(id, req.user.id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Delete('/comment/:id')
+  @HttpCode(204)
+  async unLikeComment(@Req() req, @Param('id') id: string): Promise<void> {
+    try {
+      return this.likesService.unLikeComment(id, req.user.id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Delete('/comment-reply/:id')
+  @HttpCode(204)
+  async unLikeCommentReply(@Req() req, @Param('id') id: string): Promise<void> {
+    try {
+      return this.likesService.unLikeCommentReply(id, req.user.id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
 }
--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateLikeDto } from './dto/create-like.dto';
 import { UpdateLikeDto } from './dto/update-like.dto';
+import { PrismaService } from 'src/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class LikesService {
+  constructor(
+    private prisma: PrismaService,
+  ) { }
   // create(createLikeDto: CreateLikeDto) {
   //   return 'This action adds a new like';
   // }
@@ -23,4 +28,133 @@ export class LikesService {
   // remove(id: number) {
   //   return `This action removes a #${id} like`;
   // }
+
+  // LIKE
+
+  // like itikaf
+  async likeItikaf(itikafId: string, userId: string): Promise<void> {
+    const find = await this.prisma.like.findFirst({
+      where: { itikafId, userId }
+    });
+    if (find) return;
+
+    await this.prisma.like.create({
+      data: { itikafId, userId }
+    });
+
+    return;
+  }
+
+  // like itikaf schedule
+  async likeItikafSchedule(itikafScheduleId: string, userId: string): Promise<void> {
+    const find = await this.prisma.like.findFirst({
+      where: { itikafScheduleId, userId }
+    });
+    if (find) return;
+
+    await this.prisma.like.create({
+      data: { itikafScheduleId, userId }
+    });
+
+    return;
+  }
+
+  // like article
+  async likeArticle(articleId: string, userId: string): Promise<void> {
+    const find = await this.prisma.like.findFirst({
+      where: { articleId, userId }
+    });
+    if (find) return;
+
+    await this.prisma.like.create({
+      data: { articleId, userId }
+    });
+
+    return;
+  }
+
+  // like comment
+  async likeComment(commentId: string, userId: string): Promise<void> {
+    const find = await this.prisma.like.findFirst({
+      where: { commentId, userId }
+    });
+    if (find) return;
+
+    await this.prisma.like.create({
+      data: { commentId, userId }
+    });
+
+    return;
+  }
+
+  // like comment reply
+  async likeCommentReply(commentReplyId: string, userId: string): Promise<void> {
+    const find = await this.prisma.like.findFirst({
+      where: { commentReplyId, userId }
+    });
+    if (find) return;
+
+    await this.prisma.like.create({
+      data: { commentReplyId, userId }
+    });
+
+    return;
+  }
+
+  // UNLIKE
+
+  // unLike itikaf
+  async unLikeItikaf(itikafId: string, userId: string): Promise<void> {
+    try {
+      await this.prisma.like.deleteMany({
+        where: { itikafId, userId }
+      });
+      return;
+    } catch (error) {
+    }
+  }
+
+  // unLike itikaf schedule
+  async unLikeItikafSchedule(itikafScheduleId: string, userId: string): Promise<void> {
+    try {
+      await this.prisma.like.deleteMany({
+        where: { itikafScheduleId, userId }
+      });
+      return;
+    } catch (error) {
+    }
+  }
+
+  // unLike itikaf schedule
+  async unLikeArticle(articleId: string, userId: string): Promise<void> {
+    try {
+      await this.prisma.like.deleteMany({
+        where: { articleId, userId }
+      });
+      return;
+    } catch (error) {
+    }
+  }
+
+  // unLike itikaf schedule
+  async unLikeComment(commentId: string, userId: string): Promise<void> {
+    try {
+      await this.prisma.like.deleteMany({
+        where: { commentId, userId }
+      });
+      return;
+    } catch (error) {
+    }
+  }
+
+  // unLike itikaf schedule
+  async unLikeCommentReply(commentReplyId: string, userId: string): Promise<void> {
+    try {
+      await this.prisma.like.deleteMany({
+        where: { commentReplyId, userId }
+      });
+      return;
+    } catch (error) {
+    }
+  }
 }
